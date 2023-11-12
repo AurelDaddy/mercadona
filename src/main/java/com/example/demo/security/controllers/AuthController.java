b03fcf10db33696ff60c3ee843e4c61ce692d33c
AurelDaddy <aurelienleroch@gmail.com>
2023-11-12 15:34:43 +0100

    ready to deploy

--- src/main/java/com/example/demo/security/controllers/AuthController.java
+++ src/main/java/com/example/demo/security/controllers/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials = "true", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
